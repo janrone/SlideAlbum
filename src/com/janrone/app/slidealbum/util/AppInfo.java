4fb27d9fabaf35e7c71532d379d56f4fde06ce9f
janrone <janrone@qq.com>
2016-04-14 15:51:52 +0800

    更新1.2 版本

---
+++ src/com/janrone/app/slidealbum/util/AppInfo.java
@@ -0,0 +1,8 @@
+package com.janrone.app.slidealbum.util;
+
+public class AppInfo {
+	// dropbox app key  and secret     
+	static String APP_KEY = "n4po7l1pqjp7exy";
+	static String APP_SECRET = "to0d4p6pb097rql";
+	
+}
